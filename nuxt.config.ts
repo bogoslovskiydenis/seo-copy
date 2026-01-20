70588dc5edea5908027d70b35f99ad894c9575c8
denis <bogoslovskiydenis@gmail.com>
2026-01-20 11:24:12 +0200

    добавил аналитику

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: '2-0WB6GH45b1Ylj_kWQZaH9XQHE7vGNiBy9x6Ll5Fm4'
+        }
+      ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'canonical', href: 'https://www.spinempirecasinoreview.com/' }
